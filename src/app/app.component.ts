a6d10dca74a245204a61707813c2ac05aa47995b
Felix Trepanier <felixtrepanier@gmail.com>
2022-10-09 15:50:49 -0400

    v1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { TranslocoService } from '@ngneat/transloco';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'traduction';
+  FR_CA = 'fr-CA';
+  EN_CA = 'en-CA';
+  LOCALE_KEY = 'locale';
+
+  amount = 123.34;
+  date = new Date();
+  selectedLocale = this.FR_CA;
+
+  constructor(private translocoService: TranslocoService) {
+    this.selectedLocale = localStorage.getItem(this.LOCALE_KEY) ?? this.FR_CA;
+    this.translocoService.setActiveLang(this.selectedLocale);
+  }
+
+  selectChange() {
+    localStorage.setItem(this.LOCALE_KEY, this.selectedLocale);
+    this.translocoService.setActiveLang(this.selectedLocale);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { TranslocoRootModule } from './transloco-root.module';
+import { FormsModule } from '@angular/forms';
+
+import { registerLocaleData } from '@angular/common';
+import localeFrCa from '@angular/common/locales/fr-CA';
+
+// On en a besoin pour les pipes Angular
+registerLocaleData(localeFrCa);
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, HttpClientModule, TranslocoRootModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
